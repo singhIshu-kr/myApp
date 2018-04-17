508ae95069af62b6a9b0c14c9a9f8f22f35a8ef9
Ishu Singh <ishus1242@gmail.com>
2018-04-17 16:03:11 +0530

    added git handler

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app=express();
 const PORT= process.env.PORT || 8000;
-
+const fs = require('fs');
+app.use(express.json());
 
 app.get('/',(req,res)=>{
-  res.send("hello..");
+  res.send(fs.readFileSync("file.txt","utf8"));
 })
+
+app.post('/git',(req,res)=>{
+  fs.appendFileSync(req.body,'file.txt');
+  res.end();
+})
+
 app.listen(PORT);
